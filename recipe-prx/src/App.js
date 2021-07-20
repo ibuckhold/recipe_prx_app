80b265544ba538da933aa31a7400dad5d4b07819
Icarus <ibuckhold@gmail.com>
2021-07-20 11:48:17 -0600

    api set up

--- recipe-prx/src/App.js
+++ recipe-prx/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const APP_ID = 'dad6c7a5';
+  const APP_KEY = '5be23a473db3886d58619a523b4bb28f';
+  const example = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+
   return (
     <div className="App">
       <h1>Hello</h1>
