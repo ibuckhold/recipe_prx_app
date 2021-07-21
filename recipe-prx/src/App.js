2e4c1235c5e240cfa51b9d8d1ee3a11c70d4bdb6
Icarus <ibuckhold@gmail.com>
2021-07-21 10:43:58 -0600

    form set up

--- recipe-prx/src/App.js
+++ recipe-prx/src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   const APP_ID = 'dad6c7a5';
   const APP_KEY = '5be23a473db3886d58619a523b4bb28f';
-  const example = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+  const example = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <form></form>
     </div>
   );
 }
